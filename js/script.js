5cabe382374d75d5fe8f6036554b69d2a4ca2167
Resul Elezi <resul_elezi@protonmail.com>
2024-11-08 15:59:11 +0100

    #

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const TASK_INPUT = document.getElementById("task-input");
-const TASK_INPUT = document.getElementById("task-input");
-const TASK_INPUT = document.getElementById("task-input");
-const TASK_INPUT = document.getElementById("task-input");
-const TASK_INPUT = document.getElementById("task-input");
-const TASK_INPUT = document.getElementById("task-input");
+const TASK_BUTTON = document.getElementById("task-btn");
+const TASK_LIST = document.getElementById("task-list");
+const FILTER_ALL = document.getElementById("filter-all");
+const FILTER_PEND = document.getElementById("filter-pending");
+const FILTER_COMP = document.getElementById("filter-completed");
