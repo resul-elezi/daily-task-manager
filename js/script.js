b9406f2ef61157617e2b02baea9daa70e29ca043
Resul Elezi <resul_elezi@protonmail.com>
2024-11-08 14:54:00 +0100

    #

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const TASK_INPUT = document.getElementById("task-input");
+const TASK_INPUT = document.getElementById("task-input");
+const TASK_INPUT = document.getElementById("task-input");
+const TASK_INPUT = document.getElementById("task-input");
+const TASK_INPUT = document.getElementById("task-input");
+const TASK_INPUT = document.getElementById("task-input");
